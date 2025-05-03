d6cad375f101ca652005cde3344221669899ed87
cetinyavuz1 <cetinyavuz1@hotmail.com>
2025-05-03 17:54:31 +0300

    03.05.2025
    Ufak değişiklikler yapıldı test yazıldı

--- cypress/e2e/pizza.cy.js
+++ cypress/e2e/pizza.cy.js
@@ -1,9 +1,31 @@
-describe('Siparistest', () => {
+describe('Siparis Test', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:5174/Siparis')
+    cy.visit('http://localhost:5173/SiparisForm')
   })
   it("inputa bir metin giren test",() =>{
-    const testNotu = "Canımsın"
+    const testNotu = "test"
     cy.get(`textArea[name="text"]`).type(testNotu).should("have.value", testNotu)
   })
+  it("birden fazla malzeme testi", () =>{
+    const testTopping = ["Sucuk", "Salam", "Mısır"]
+    testTopping.forEach((elm) => {
+      cy.get(`input[type="checkbox"][value="${elm}"]`).check().should("be.checked")
+    })
+  })
+  it("form doldurulup gönderiliyor mu?", () => {
+    cy.get(`input[type="radio"][value="Büyük"]`).check().should("be.checked")
+
+    cy.get(`select#exampleSelect`).select("İnce")
+
+    const testTopping = ["Sucuk", "Salam", "Mısır", "Pepperoni"]
+    testTopping.forEach((elm) => {
+      cy.get(`input[type="checkbox"][value="${elm}"]`).check().should("be.checked")
+    })
+
+    const testNotu = "test"
+    cy.get(`textArea[name="text"]`).type(testNotu).should("have.value", testNotu)
+
+    cy.get(`button[type="submit"]`).click()
+  })
 })
+
--- src/components/SiparisHeader.jsx
+++ src/components/SiparisHeader.jsx
@@ -1,5 +1,6 @@
 import React from "react"
 import styled from "styled-components"
+import { Link } from "react-router-dom/cjs/react-router-dom.min"
 
 
 const StyledHeader = styled.header`
@@ -10,11 +11,40 @@ const StyledHeader = styled.header`
     background-position: center
     `
 
+const StyledDiv= styled.div`
+display:flex;
+flex-direction: row;
+justify-content: center;
+height: 20vh;
+align-items: flex-end;
+color: white
+`
+
+const StyledButton1= styled.button`
+background-color: #CE2829;
+border: 1px solid #CE2829;
+color: white;
+font-family: "Barlow";
+font-weight: 400
+`
+
+const StyledButton2= styled.button`
+background-color: #CE2829;
+border: 1px solid #CE2829;
+color: white;
+font-family: "Barlow";
+font-weight: 700
+`
+
 export default function SiparisHeader() {
 
     return (
         <StyledHeader>
-            
+            <StyledDiv>
+               <Link to="/"><StyledButton1>Ana Sayfa</StyledButton1></Link>
+                -
+               <Link><StyledButton2>Sipariş Oluştur</StyledButton2></Link>
+            </StyledDiv>
         </StyledHeader>
     )
 }
